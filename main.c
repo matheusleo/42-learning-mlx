582977e4d2dcfe3a482b1e23926a032d043c9191
matheusleo <matheusleotrab@gmail.com>
2022-09-09 09:51:28 -0300

    introduction - pushin images to a window
    after creating a image and writing to it, understand how we push that image to the window

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 20:58:30 by mleonard          #+#    #+#             */
-/*   Updated: 2022/09/09 09:41:31 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/09/09 09:50:27 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,15 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 int	main(void)
 {
 	void	*mlx;
+	void	*mlx_win;
 	t_data	img;
 
 	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 960, 640, "Hello world!");
 	img.img = mlx_new_image(mlx, 960, 540);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
 					&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
