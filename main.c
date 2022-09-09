9e17f1191e5f44781a9464399618cc428684f6f0
matheusleo <matheusleotrab@gmail.com>
2022-09-09 09:42:38 -0300

    introduction - writing pixel to a image
    create a basic function to understand how to write pixels to a window and how the other mlx functions works

--- main.c
+++ main.c
@@ -6,19 +6,38 @@
 /*   By: mleonard <mleonard@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/05 20:58:30 by mleonard          #+#    #+#             */
-/*   Updated: 2022/09/05 23:24:03 by mleonard         ###   ########.fr       */
+/*   Updated: 2022/09/09 09:41:31 by mleonard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mlx.h>
 
-int main(void)
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	int		offset;
+	char	*dst;
+
+	offset = (y * data->line_length + x * (data->bits_per_pixel / 8));
+	dst = data->addr + offset;
+	*(unsigned int *)dst = color;
+}
+
+int	main(void)
 {
 	void	*mlx;
-	void	*mlx_win;
+	t_data	img;
 
 	mlx = mlx_init();
-	
-	mlx_win = mlx_new_window(mlx, 960, 540, "Hello world!");
-	mlx_loop(mlx);
+	img.img = mlx_new_image(mlx, 960, 540);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+					&img.line_length, &img.endian);
 }
